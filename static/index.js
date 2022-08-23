f4e3a70403539347503ab5ef70660b92e79e5063
Jesse Perez <jesseprn@gmail.com>
2022-08-22 21:01:13 -0400

    getting flask to work with RDS instance

--- static/index.js
+++ static/index.js
@@ -5,11 +5,28 @@ function playerSelected() {
   let dropdownMenu = d3.select("#selectPlayer");
   let player = dropdownMenu.property("value");
   console.log("crypto ticker selected value is " + player)
-  d3.json("/tickers/name/" + tickerName).then(function (response) {
-    console.log("playerSelected response below");
-    console.log(response);
-  })
+  // d3.json("/tickers/name/" + tickerName).then(function (response) {
+  //   console.log("playerSelected response below");
+  //   console.log(response);
+  // })
+  addSelectedPlayer(player)
 }
+
+function addSelectedPlayer(player) {
+  // console.log("addTickerSelected being executed on " + ticker)
+  d3.select("#userRoster")
+    .append("button")
+    .html(player)
+    .attr("class", "player")
+    .on("click", function (d) {
+      // console.log(d);
+      // console.log(i);
+      // let testval = d3.event.target.childNodes[0].data
+      console.log(d.target.childNodes[0].data);
+    })
+
+}
+
 
 init();
 
@@ -34,3 +51,10 @@ function init() {
   // dropdownMenu.property("value") = "Netflix"
 
 }//end init
+
+//utility
+function resetLines() {
+  // console.log("resetlines executing")
+  d3.select('#userRoster').selectAll("*").remove()
+  tickerData = []
+}
