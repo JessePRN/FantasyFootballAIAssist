fde48df33ce2898ede41c325b691b8449f4ac4cb
unknown <yhuang7802@gmail.com>
2022-08-31 00:06:03 -0400

    added the web page to the main folder

--- Yi/static/js/index.js
+++ Yi/static/js/index.js
@@ -1,16 +1,12 @@
 playerList = []
+playerListDisplay = []
 userRoster = []
 
 // called by player dropdown
 function playerSelected() {
   let dropdownMenu = d3.select("#selectPlayer");
   let player = dropdownMenu.property("value");
-  console.log("crypto ticker selected value is " + player)
-  // d3.json("/tickers/name/" + tickerName).then(function (response) {
-  //   console.log("playerSelected response below");
-  //   console.log(response);
-  // })
-  addSelectedPlayer(player)
+  console.log("selected player is " + player)
 }
 
 function addSelectedPlayer(player) {
@@ -20,13 +16,9 @@ function addSelectedPlayer(player) {
     .html(player)
     .attr("class", "player")
     .on("click", function (d) {
-      // console.log(d);
-      // console.log(i);
-      // let testval = d3.event.target.childNodes[0].data
       console.log(d.target.childNodes[0].data);
       console.log(searchPlayerList((d.target.childNodes[0].data).split(",")[0]))
     })
-
 }
 
 init();
@@ -42,148 +34,175 @@ function init() {
     for (var i = 0; i < response.length; i++) {
       var obj = response[i]
       d3.select('#selectPlayer')
-          .append('option')
-          .text(obj.player) 
+        .append('option')
+        .text(obj.player)
     }
   })
 
   d3.json("/players/all").then(function (response) {
-    console.log("/players/all");
-    console.log(response);
-    // response = response.sort()
-    // buildList(response)
+    // console.log("/players/all");
+    // console.log(response);
     playerList = response
-    for (var i = 0; i < response.length; i++) {
-      var obj = response[i]
-      d3.select('#playerList')
-          .append('li')
-          .attr('class', 'list-group-item')
-          .text(obj.player + ", Points: " + obj.points2021 + ", POS:" + obj.pos) 
-    }
+    filterBuildTableClean("All")
+    handlePagination()
   })
 
   // displaying radius chart
   drawRadialChart('All')
   console.log("display radial chart")
 
+
 }//end init
 
 //utility
 function resetLines() {
   // console.log("resetlines executing")
-  d3.select('#selectPlayer').selectAll("*").remove()
+  d3.select('#userRoster').selectAll("*").remove()
   tickerData = []
 }
 
-// function filterBuildTable(pos){
-//   d3.select('#playerList').selectAll("*").remove()
-
-//   if (pos != 'All'){ 
-//     for (var i = 0; i < playerList.length; i++) {
-//       var obj = playerList[i]
-//       if(playerList[i].pos === pos){
-//       d3.select('#playerList')
-//           .append('li')
-//           .attr('class', 'list-group-item')
-//           .text(obj.player + ", Points: " + obj.points2021 + ", POS:" + obj.pos) 
-//         }
-//     }
-//   }
-//   else {
-
-//   }
-// }
-
-function filterBuildTable(pos){
-  d3.select('#playerList').selectAll("*").remove()
-
+//takes a player name and searches list of players for player data
+function searchPlayerList(player) {
   for (var i = 0; i < playerList.length; i++) {
     var obj = playerList[i]
-    if(playerList[i].pos === pos){
-    d3.select('#playerList')
-        .append('li')
-        .attr('class', 'list-group-item')
-        .text(obj.player + ", Points: " + obj.points2021 + ", POS:" + obj.pos) 
-      }
+    if (player === obj.player) {
+      return obj
+    }
   }
 }
 
+function filterBuildTableClean(pos) {
+  d3.select('#playerTable').select('tbody').selectAll("*").remove()
+  playerListDisplay = []
 
-// scratch
-// Append an option in the dropdown
-      // response.forEach(function (player) {
-      //   d3.select('#selectPlayer')
-      //     .append('option')
-      //     .text(player)
-      // });
+  for (var i = 0; i < playerList.length; i++) {
+    var obj = playerList[i]
+    if (pos === "All" || obj.pos === pos) {
 
+      row = d3.select('#playerTable').select('tbody').append("tr")
+      // .data(obj)
+      // .enter().append("tr")
+      row.append('td').text(obj.player)
+      row.append('td').text(obj.points2021)
+      row.append('td').text(obj.pos)
+      row.append('td').text(obj.production.toFixed(3))
+      row.append('td').text(obj.atp.toFixed(3))
+      row.append('td').text(obj.team)
+      row.append('td').text(obj.avg)
+      row.append('td').text(obj.pred.toFixed(3))
+      row.append('td').append("button").attr("value", obj.player).text("+").on("click", function (d) {
+        console.log(d.target.value)
+        addSelectedPlayer((d.target.value))
+      })
+      playerListDisplay.push(obj)
+    }
+  }
+}
 
-function drawRadialChart(pos){
-console.log('draw radial chart is working?')
-// set the dimensions and margins of the graph
-const margin = {top: 0, right: 0, bottom: 0, left: -250},
+function handlePagination() {
+  d3.select('#nav').selectAll("*").remove()
+
+  $(document).ready(function () {
+    $('#playerTable').after('<div id="nav"></div>');
+    var rowsShown = 10;
+    var rowsTotal = $('#playerTable tbody tr').length;
+    var numPages = rowsTotal / rowsShown;
+    for (i = 0; i < numPages; i++) {
+      var pageNum = i + 1;
+      $('#nav').append('<a href="#" rel="' + i + '">' + pageNum + '</a> ');
+    }
+    $('#playerTable tbody tr').hide();
+    $('#playerTable tbody tr').slice(0, rowsShown).show();
+    $('#nav a:first').addClass('active');
+    $('#nav a').bind('click', function () {
+
+      $('#nav a').removeClass('active');
+      $(this).addClass('active');
+      var currPage = $(this).attr('rel');
+      var startItem = currPage * rowsShown;
+      var endItem = startItem + rowsShown;
+      $('#playerTable tbody tr').css('opacity', '0.0').hide().slice(startItem, endItem).
+        css('display', 'table-row').animate({ opacity: 1 }, 300);
+    })
+  })
+}
+// drawing the radial chart
+
+function drawRadialChart(pos) {
+  console.log('draw radial chart is working?')
+  // set the dimensions and margins of the graph
+  const margin = { top: 0, right: 0, bottom: 0, left: -250 },
     width = 1500 - margin.left - margin.right,
     height = 1500 - margin.top - margin.bottom,
     innerRadius = 350,
     outerRadius = Math.min(width, height) / 2.5;   // the outerRadius goes from the middle of the SVG area to the border
 
-// append the svg object
-const svg = d3.select("#radialChart")
-  .html("")
-  .append("svg")
+  // append the svg object
+  const svg = d3.select("#radialChart")
+    .html("")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", `translate(${width/2+margin.left}, ${height/2+margin.top})`);
+    .append("g")
+    .attr("transform", `translate(${width / 2 + margin.left}, ${height / 2 + margin.top})`);
 
-// accessing player data
-d3.json('/draft').then( function(data) {
-  if (pos != 'All'){
-    data = data.filter(d => {return d.Pos == pos})
-  }
-  //sort
-  data = data.sort((a,b) =>{ return b.Prediction - a.Prediction} )
+  // accessing player data
+  d3.json("/players/all").then(function (data) {
+    // d3.json("/draft").then(function (data) {
+    // console.log("draft response")
+    // console.log(data)
 
-  // Scales
-  const x = d3.scaleBand()
+    if (pos != 'All') {
+      data = data.filter(d => { return d.pos == pos })
+    }
+    //sort
+    data = data.sort((a, b) => { return b.pred - a.pred })
+
+    // Scales
+    const x = d3.scaleBand()
       .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
       .align(0)                  // This does nothing
-      .domain(data.map(d => d.Player)); // The domain of the X axis is the list of states.
-  const y = d3.scaleRadial()
+      .domain(data.map(d => d.player)); // The domain of the X axis is the list of states.
+    const y = d3.scaleRadial()
       .range([innerRadius, outerRadius])   // Domain will be define later.
       .domain([0, 14000]); // Domain of Y is from 0 to the max seen in the data
 
-  // Add the bars
-  svg.append("g")
-    .selectAll("path")
-    .data(data)
-    .join("path")
+    // Add the bars
+    svg.append("g")
+      .selectAll("path")
+      .data(data)
+      .join("path")
       .attr("fill", "#69b3a2")
       .attr("d", d3.arc()     // imagine your doing a part of a donut plot
-          .innerRadius(innerRadius)
-          .outerRadius(d => y(15*d['Prediction']))
-          .startAngle(d => x(d.Player))
-          .endAngle(d => x(d.Player) + x.bandwidth())
-          .padAngle(0.01)
-          .padRadius(innerRadius))
+        .innerRadius(innerRadius)
+        .outerRadius(d => y(15 * d['pred']))
+        .startAngle(d => x(d.player))
+        .endAngle(d => x(d.player) + x.bandwidth())
+        .padAngle(0.01)
+        .padRadius(innerRadius))
 
-  // Add the labels
-  svg.append("g")
+    // Add the labels
+    svg.append("g")
       .selectAll("g")
       .data(data)
       .join("g")
-        .attr("text-anchor", function(d) { return (x(d.Player) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-        .attr("transform", function(d) { return "rotate(" + ((x(d.Player) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Prediction'])+90) + ",0)"; })
+      .attr("text-anchor", function (d) { return (x(d.player) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+      .attr("transform", function (d) { return "rotate(" + ((x(d.player) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")" + "translate(" + (y(d['pred']) + 90) + ",0)"; })
       .append("text")
-        .text(function(d){return(`${d.Player}: ${d.AVG}`)})
-        .attr("transform", function(d) { return (x(d.Player) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
-        .style("font-size", "11px")
-        .attr("alignment-baseline", "middle")
+      .text(function (d) { return (`${d.player}: ${d.avg}`) })
+      .attr("transform", function (d) { return (x(d.player) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+      .style("font-size", "11px")
+      .attr("alignment-baseline", "middle")
 
-})};
+    console.log("confirm chart is graphed")
+  })
+};
 
-function updateAllCharts(pos){
-  filterBuildTable(pos)
+// function to update all the charts 
+function updateAllCharts(pos) {
+  filterBuildTableClean(pos)
+  handlePagination()
   drawRadialChart(pos)
-  
+
+  console.log("confirm updating all charts")
 }
